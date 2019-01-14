d3844a63623f9e0d3b5bbebadd9c45146bb350aa
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-13 18:20:22 -0800

    Created Node Class

--- Challenges/LinkedList/LinkedList/Classes/Node.cs
+++ Challenges/LinkedList/LinkedList/Classes/Node.cs
@@ -2,9 +2,20 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace LinkedList
+namespace LinkedList.Classes
 {
+    /// <summary>
+    /// When instantiated creates a node object with a integer value and references to previous and next node.
+    /// </summary>
     class Node
     {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+        public Node Previous { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
     }
 }
