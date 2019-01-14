b151e5b14067ee51ea173ee07f1e653d5c461bcc
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-13 19:42:41 -0800

    Completed LList.Print tests

--- Challenges/LinkedList/LinkedListTest/UnitTest1.cs
+++ Challenges/LinkedList/LinkedListTest/UnitTest1.cs
@@ -111,19 +111,50 @@ namespace LinkedListTest
     public class TestingPrint
     {
         [Fact]
-        public void Test1()
+        public void TestingThreeNodeLinkedList()
         {
+            //Makes a new linked list
+            LList test = new LList();
 
+            //Inserts new node into linked list.
+            test.InsertAtHead(1);
+            test.InsertAtHead(2);
+            test.InsertAtHead(3);
+
+            //prints out an array with all nodes in list starting with head value.
+            int[] actualValues = test.PrintValues();
+            int[] expectedValues = new int[] { 3, 2, 1 };
+            //Checks to see if expected equals actual.
+            Assert.Equal(expectedValues, actualValues);
         }
         [Fact]
-        public void Test2()
+        public void TestingThreeIdenticalNodeLinkedList()
         {
+            //Makes a new linked list
+            LList test = new LList();
 
+            //Inserts new node into linked list.
+            test.InsertAtHead(1);
+            test.InsertAtHead(1);
+            test.InsertAtHead(1);
+
+            //prints out an array with all nodes in list starting with head value.
+            int[] actualValues = test.PrintValues();
+            int[] expectedValues = new int[] { 1, 1, 1 };
+            //Checks to see if expected equals actual.
+            Assert.Equal(expectedValues, actualValues);
         }
         [Fact]
-        public void Test3()
+        public void TestingEmptyLinkedList()
         {
-
+            //Makes a new linked list
+            LList test = new LList();
+            
+            //prints out an array with all nodes in list starting with head value.
+            int[] actualValues = test.PrintValues();
+            int[] expectedValues = new int[] {};
+            //Checks to see if expected equals actual.
+            Assert.Equal(expectedValues, actualValues);
         }
     }
 }
