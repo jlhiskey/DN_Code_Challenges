16f077955e527faa26f0dcc7ab569ae0a9f12d6f
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-21 11:08:14 -0800

    Completed Node class

--- DataStructures/StacksAndQueues/StacksAndQueues/StacksAndQueues/Classes/Node.cs
+++ DataStructures/StacksAndQueues/StacksAndQueues/StacksAndQueues/Classes/Node.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace StacksAndQueues.Classes
 {
+    /// <summary>
+    /// Creates a node with a value. When creating a new node you must give it a value.
+    /// </summary>
     class Node
     {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+
     }
+    
 }
