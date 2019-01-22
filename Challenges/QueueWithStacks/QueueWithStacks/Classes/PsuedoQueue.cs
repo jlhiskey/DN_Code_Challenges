9c9e46a37f3e165fe96934734d79edca0eaf725f
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-22 11:43:06 -0800

    Added basic framework to PsuedoQueue

--- Challenges/QueueWithStacks/QueueWithStacks/Classes/PsuedoQueue.cs
+++ Challenges/QueueWithStacks/QueueWithStacks/Classes/PsuedoQueue.cs
@@ -7,8 +7,27 @@ namespace QueueWithStacks.Classes
 {
     class PsuedoQueue
     {
+        Stack stackOne = new Stack();
+        Stack stackTwo = new Stack();
 
+        public Node Front { get; set; }
+        public Node Back { get; set; }
 
-        
+        public PsuedoQueue(Node node)
+        {
+            Front = node;
+            Back = node;
+            
+        }
+
+        public int Enqueue(Node node)
+        {
+
+        }
+
+        public int Dequeue(Node node)
+        {
+
+        }
     }
 }
