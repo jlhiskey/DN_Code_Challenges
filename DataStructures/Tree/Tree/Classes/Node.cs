289beb87cb893367d1333dac253f77fc6a695e70
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-25 09:48:34 -0800

    Completed Node class.

--- DataStructures/Tree/Tree/Classes/Node.cs
+++ DataStructures/Tree/Tree/Classes/Node.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Tree.Classes
 {
-    class Node
+    public class Node
     {
+        public int Value { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+
     }
 }
