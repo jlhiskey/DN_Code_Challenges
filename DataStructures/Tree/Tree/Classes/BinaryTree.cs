dd989542e6b869a685414d5dbf266159ddf18a76
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-25 10:01:43 -0800

    Framed out BinaryTree class

---
+++ DataStructures/Tree/Tree/Classes/BinaryTree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree.Classes
+{
+    class BinaryTree
+    {
+        Node Root { get; set; }
+        
+        public BinaryTree()
+        {
+            
+        }
+
+        public int Add()
+        {
+
+        }
+
+        public bool Contains()
+        {
+
+        }
+
+        public int[] PreOrder()
+        {
+
+        }
+
+        public int[] InOrder()
+        {
+
+        }
+
+        public int[] PostOrder()
+        {
+
+        }
+    }
+}
