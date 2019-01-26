698aac7838fb01973fd8e52f72039cb4e5cf617a
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-26 10:00:58 -0800

    Completed PreOrder

--- DataStructures/Tree/Tree/Classes/BinaryTree.cs
+++ DataStructures/Tree/Tree/Classes/BinaryTree.cs
@@ -12,15 +12,22 @@ namespace Tree.Classes
         {
             
         }
-
-        public Traversal()
-        {
-
-        }
         
-        public int[] PreOrder()
+        public List<int> PreOrder(Node root)
         {
-
+            List<int> preOrderValues = new List<int>();
+            void PreOrderHelper(Node helperRoot, List<int> listOfValues)
+            {
+                if (root == null)
+                {
+                    return;
+                }
+                listOfValues.Add(root.Value);
+                PreOrderHelper(root.Left, listOfValues);
+                PreOrderHelper(root.Right, listOfValues);
+            }
+            PreOrderHelper(root, preOrderValues);
+            return preOrderValues;
         }
 
         public int[] InOrder()
