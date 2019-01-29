92fba946ce0b01942c76981648148d0a7e2847c7
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-28 18:52:03 -0800

    Framed out method, classes and tests.

---
+++ Challenges/FizzBuzzTree/FizzBuzzTestSuite/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using FizzBuzzTree.Classes;
+
+
+namespace FizzBuzzTestSuite
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Challenges/FizzBuzzTree/FizzBuzzTree/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FizzBuzzTree.Classes;
+
+namespace FizzBuzzTree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static BinaryTree FizzBuzzTree(BinaryTree tree)
+        
+    }
+}
