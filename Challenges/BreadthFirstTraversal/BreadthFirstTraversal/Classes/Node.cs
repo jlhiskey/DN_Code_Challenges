6e4aa814bb4342a3c8976d9e50e99d604468eea0
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-30 06:52:31 -0800

    Framed out basic classes and test proj.

---
+++ Challenges/BreadthFirstTraversal/BreadthFirstTraversal/Classes/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues.Classes
+{
+    /// <summary>
+    /// Creates a node with a value. When creating a new node you must give it a value.
+    /// </summary>
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+
+    }
+    
+}
---
+++ Challenges/BreadthFirstTraversal/BreadthFirstTraversalTestSuite/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using StacksAndQueues.Classes;
+using BreadthFirstTraversal;
+
+namespace BreadthFirstTraversalTestSuite
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
