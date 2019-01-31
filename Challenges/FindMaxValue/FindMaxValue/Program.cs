38eb96feadc498600854b8c841a1089baa5cf213
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-31 07:45:13 -0800

    Framed out program and test projects and brought in tree.dll as a dependency.

---
+++ Challenges/FindMaxValue/FindMaxValue/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Tree.Classes;
+
+namespace FindMaxValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Challenges/FindMaxValue/FindMaxValueTestSuite/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Tree.Classes;
+using Xunit;
+
+namespace FindMaxValueTestSuite
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
