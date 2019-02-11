4dc76a783794f14617a467ac95e670803336de5b
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-11 15:14:57 -0800

    Completed AreCharactersUnique now writing tests.

---
+++ Challenges/AreCharactersUnique/AreCharactersUnique/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AreCharactersUnique
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing: ABCD");
+            Console.WriteLine(AreCharactersUnique("ABCD"));
+
+            Console.WriteLine("Testing: ABCA");
+            Console.WriteLine(AreCharactersUnique("ABCA"));
+
+            Console.WriteLine("Testing: AabCD");
+            Console.WriteLine(AreCharactersUnique("AabCD"));
+
+            Console.ReadLine();
+        }
+
+        public static bool AreCharactersUnique(string input)
+        {
+            Dictionary<char, char> visitedCharacters = new Dictionary<char, char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (visitedCharacters.ContainsKey(input[i]))
+                {
+                    return false;
+                }
+                visitedCharacters.Add(input[i], input[i]);
+            }
+            return true;
+        }
+    }   
+}
