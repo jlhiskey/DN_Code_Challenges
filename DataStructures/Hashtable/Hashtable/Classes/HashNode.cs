2c6af25336377f945e4708ffcd92f88a1597e96c
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-13 08:55:03 -0800

    Created a HashNode and started building my hashtable

---
+++ DataStructures/Hashtable/Hashtable/Classes/HashNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hashtable.Classes
+{
+    public class HashNode
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public HashNode Next { get; set; }
+
+        public HashNode(string key, string value)
+        {
+            Key = key;
+            Value = value;
+            Next = null;
+        }
+    }
+}
+ 
