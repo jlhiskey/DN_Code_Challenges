40a33bee53292540b7a4f2384c73ea170408342e
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-15 07:30:02 -0800

    Completed challenge and testing

--- Challenges/InsertionSort/InsertionSort/Program.cs
+++ Challenges/InsertionSort/InsertionSort/Program.cs
@@ -11,7 +11,27 @@ namespace InsertionSort
 
         public static int[] InsertionSort(int[] array)
         {
-
+            for (int i = 0; i < array.Length; i++)
+            {
+                int j = 0;
+                if (i > 0)
+                {
+                    j = i - 1;
+                };
+                 
+                int target = array[i];
+                int previous = array[j];
+                while (target < previous && j >= 0)
+                {
+                    array[j] = target;
+                    array[j + 1] = previous;
+                    if (j > 0)
+                    {
+                        previous = array[j - 1];
+                    }   
+                    j = j - 1;
+                }
+            }
             return array;
         }
     }
