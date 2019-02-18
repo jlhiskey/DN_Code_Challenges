1a1969455dfcc710c79dc76766ea96d00a7a7dca
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-18 14:11:46 -0800

    Framed out tests and basic class

---
+++ Challenges/ConsoleApp1/RepeatedWord/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RepeatedWord
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string RepeatedWord(string book)
+        {
+            return "no repeated words";
+        }
+    }    
+}
---
+++ Challenges/ConsoleApp1/RepeatedWordTestSuite/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+using RepeatedWord;
+
+namespace RepeatedWordTestSuite
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal("a", Program.RepeatedWord("A volcano is a volcano."));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal("party", Program.RepeatedWord("When is the Party? What party?"));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Assert.Equal("no repeated words", Program.RepeatedWord("Today is monday"));
+        }
+    }
+}
