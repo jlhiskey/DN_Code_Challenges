9388edf334f7e4d12d4dea19b1e1142e03359693
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-19 15:08:17 -0800

    Added missing tests and reordered tests to match canvas assignment test order.

--- Challenges/InsertionSort/InsertionSortTestSuite/InsertionSortTestSuite.cs
+++ Challenges/InsertionSort/InsertionSortTestSuite/InsertionSortTestSuite.cs
@@ -9,7 +9,7 @@ namespace InsertionSortTestSuite
         [Fact]
         public void TestInsertionSort1()
         {
-            int[] testArray = new int[] { 5, 4, 3, 2, 1 };
+            int[] testArray = new int[] { 1, 3, 5, 2, 4 };
 
             int[] expected = new int[] { 1, 2, 3, 4, 5 };
 
@@ -19,7 +19,7 @@ namespace InsertionSortTestSuite
         [Fact]
         public void TestInsertionSort2()
         {
-            int[] testArray = new int[] { 1, 4, 2, 3, 5 };
+            int[] testArray = new int[] { 1, 2, 3, 4, 5 };
 
             int[] expected = new int[] { 1, 2, 3, 4, 5 };
 
@@ -29,9 +29,29 @@ namespace InsertionSortTestSuite
         [Fact]
         public void TestInsertionSort3()
         {
-            int[] testArray = new int[] { 1, 2, 3, 4, 5 };
+            int[] testArray = new int[] { 5, 4, 3, 2, 1 };
 
             int[] expected = new int[] { 1, 2, 3, 4, 5 };
+
+            Assert.Equal(expected, Program.InsertionSort(testArray));
+        }
+
+        [Fact]
+        public void TestInsertionSort4()
+        {
+            int[] testArray = new int[]{};
+
+            int[] expected = new int[] {};
+
+            Assert.Equal(expected, Program.InsertionSort(testArray));
+        }
+
+        [Fact]
+        public void TestInsertionSort5()
+        {
+            int[] testArray = new int[]{1};
+
+            int[] expected = new int[] {1};
 
             Assert.Equal(expected, Program.InsertionSort(testArray));
         }
