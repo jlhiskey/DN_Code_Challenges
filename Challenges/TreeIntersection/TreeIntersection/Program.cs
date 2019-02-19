b79edcd4b1bb93e27752fbb7e4b515a8717d5167
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-19 13:32:48 -0800

    Framed out files.

---
+++ Challenges/TreeIntersection/TreeIntersection/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Tree.Classes;
+using Hashtable.Classes;
+
+namespace TreeIntersection
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public List<int> TreeIntersection(BinaryTree treeOne, BinaryTree treeTwo)
+    {
+        HashTable visitedValues = new HashTable(20);
+        List<int> commonValues = new List<int>();
+
+        void TraversalOne(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            visitedValues.Add(root.Value.ToString(), null);
+
+
+        }
+    }
+}
---
+++ Challenges/TreeIntersection/TreeIntersectionTestSuite/TreeIntersectionTestSuite.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+using Tree.Classes;
+using TreeIntersection;
+
+namespace TreeIntersectionTestSuite
+{
+    public class TreeIntersectionTestSuite
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
