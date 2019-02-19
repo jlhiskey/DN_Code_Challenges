50be8065941588298260c4e786fd35d42e009f6d
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-19 14:12:48 -0800

    Completed Testing

--- Challenges/TreeIntersection/TreeIntersectionTestSuite/TreeIntersectionTestSuite.cs
+++ Challenges/TreeIntersection/TreeIntersectionTestSuite/TreeIntersectionTestSuite.cs
@@ -2,6 +2,7 @@ using System;
 using Xunit;
 using Tree.Classes;
 using TreeIntersection;
+using System.Collections.Generic;
 
 namespace TreeIntersectionTestSuite
 {
@@ -10,19 +11,58 @@ namespace TreeIntersectionTestSuite
         [Fact]
         public void Test1()
         {
+            BinaryTree testTreeOne = new BinaryTree();
+            testTreeOne.Root = new Node(1);
+            testTreeOne.Root.Left = new Node(2);
+            testTreeOne.Root.Right = new Node(3);
 
+            BinaryTree testTreeTwo = new BinaryTree();
+            testTreeTwo.Root = new Node(4);
+            testTreeTwo.Root.Left = new Node(1);
+            testTreeTwo.Root.Right = new Node(2);
+
+            List<int> expected = new List<int> { 1, 2 };
+            List<int> actual = Program.TreeIntersection(testTreeOne, testTreeTwo);
+
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
-        public void Test1()
+        public void Test2()
         {
+            BinaryTree testTreeOne = new BinaryTree();
+            testTreeOne.Root = new Node(1);
+            testTreeOne.Root.Left = new Node(2);
+            testTreeOne.Root.Left.Left = new Node(3);
 
+            BinaryTree testTreeTwo = new BinaryTree();
+            testTreeTwo.Root = new Node(1);
+            testTreeTwo.Root.Left = new Node(2);
+            testTreeTwo.Root.Right = new Node(3);
+
+            List<int> expected = new List<int> { 1, 2, 3 };
+            List<int> actual = Program.TreeIntersection(testTreeOne, testTreeTwo);
+
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
-        public void Test1()
+        public void Test3()
         {
+            BinaryTree testTreeOne = new BinaryTree();
+            testTreeOne.Root = new Node(1);
+            testTreeOne.Root.Left = new Node(2);
+            testTreeOne.Root.Left.Left = new Node(3);
 
+            BinaryTree testTreeTwo = new BinaryTree();
+            testTreeTwo.Root = new Node(4);
+            testTreeTwo.Root.Left = new Node(5);
+            testTreeTwo.Root.Right = new Node(6);
+
+            List<int> expected = new List<int>();
+            List<int> actual = Program.TreeIntersection(testTreeOne, testTreeTwo);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
