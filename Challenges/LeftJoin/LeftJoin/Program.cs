2c14b2e0feb85bab754123a9fde00bda1803b314
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-20 15:22:13 -0800

    Framed out LeftJoin method and test file

---
+++ Challenges/LeftJoin/LeftJoin/Program.cs
@@ -0,0 +1,18 @@
+﻿using Hashtable.Classes;
+using System;
+
+namespace LeftJoin
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public List<string> LeftJoin(HashTable left, HashTable right)
+        {
+            
+        }
+    }
+}
---
+++ Challenges/LeftJoin/LeftJoinTestSuite/LeftJoinTestSuite.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LeftJoinTestSuite
+{
+    public class LeftJoinTestSuite
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
