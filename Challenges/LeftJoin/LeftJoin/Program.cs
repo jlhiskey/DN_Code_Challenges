a85cad4d89507716fa700e9b13910072d6dc180a
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-20 15:53:14 -0800

    Completed challenge

--- Challenges/LeftJoin/LeftJoin/Program.cs
+++ Challenges/LeftJoin/LeftJoin/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Hashtable.Classes;
 using System;
+using System.Collections.Generic;
 
 namespace LeftJoin
 {
@@ -10,9 +11,53 @@ namespace LeftJoin
             Console.WriteLine("Hello World!");
         }
 
-        public List<string> LeftJoin(HashTable left, HashTable right)
+        /// <summary>
+        /// Takes in two hashtables and outputs a list of all left hashtable values and any right table values that have a matching key.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns>List with join data. LeftKey, LeftValue, RightValue</returns>
+        public static List<string> LeftJoin(HashTable left, HashTable right)
         {
-            
+            //List to hold join data.
+            List<string> joinTable = new List<string>();
+
+            //Stepping through all hashnode is left hash table
+            for (int i = 0; i < left.Storage.Length; i++)
+            {
+                //If the bucket has a hashnode in it
+                if (left.Storage[i] != null)
+                {
+                    //Take the first hashnode out of the bucket.
+                    HashNode currentNode = left.Storage[i];
+                    
+                    //Runs until collision storage is null
+                    while (currentNode != null)
+                    {
+                        //Will hold the data collected from hashnode
+                        string concat = null;
+                        //Adds left hashnode key and value
+                        concat = concat + $"{currentNode.Key}, {currentNode.Value.ToString()}, ";
+                        //Checks to see if right hashtable contains matching key
+                        if (right.Contains(currentNode.Key))
+                        {
+                            // Adds right hashnodes value to concat
+                            concat = concat + right.Get(currentNode.Key).ToString();
+                        }
+                        else
+                        {
+                            //Adds NULL if right hashtable doesnt have matching key
+                            concat = concat + "NULL";
+                        }
+                        //Adds saved key value data to list.
+                        joinTable.Add(concat);
+                        //Moves to next node in collision storage.
+                        currentNode = currentNode.Next;
+                    }
+                }
+            }
+            //Returns joinTable
+            return joinTable;
         }
     }
 }
--- Challenges/LeftJoin/LeftJoinTestSuite/LeftJoinTestSuite.cs
+++ Challenges/LeftJoin/LeftJoinTestSuite/LeftJoinTestSuite.cs
@@ -1,4 +1,7 @@
+using Hashtable.Classes;
+using LeftJoin;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace LeftJoinTestSuite
@@ -6,9 +9,23 @@ namespace LeftJoinTestSuite
     public class LeftJoinTestSuite
     {
         [Fact]
-        public void Test1()
+        public void TestingWithNoCollisions()
         {
+            HashTable left = new HashTable(5);
+            left.Add("Bob", "First");
+            left.Add("Frank", "First");
 
+            HashTable right = new HashTable(5);
+            right.Add("Bob", "Second");
+            right.Add("Jim", "Second");
+
+            List<string> expected = new List<string>();
+            expected.Add("Bob, First, Second");
+            expected.Add("Frank, First, NULL");
+
+            List<string> actual = Program.LeftJoin(left, right);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
