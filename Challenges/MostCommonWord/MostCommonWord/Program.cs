4080305a9ee5e49ac6586c057c52eb09f0ea63d0
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-23 14:33:18 -0800

    Framed out application and tests and brought in HashTable dependency.

---
+++ Challenges/MostCommonWord/MostCommonWord/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MostCommonWord
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string MostCommonWord(string book)
+        {
+            string mostCommonWord = "No Words in Book";
+
+            return mostCommonWord;
+        }
+    }
+}
---
+++ Challenges/MostCommonWord/XUnitTestProject1/MostCommonWordTestSuite.cs
@@ -0,0 +1,40 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class MostCommonWordTestSuite
+    {
+        [Fact]
+        public void TestingOneDoubleRepeatingWord()
+        {
+
+
+
+        }
+
+        [Fact]
+        public void TestingTwoeDoubleRepeatingWords()
+        {
+
+
+
+        }
+
+        [Fact]
+        public void TestingAllUniqueWords()
+        {
+
+
+
+        }
+
+        [Fact]
+        public void TestingNoWords()
+        {
+
+
+
+        }
+    }
+}
