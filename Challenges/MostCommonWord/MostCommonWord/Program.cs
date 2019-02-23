28fab6af251dff261c20f383662355ae1e262cdb
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-23 15:24:18 -0800

    Completed Challenge and testing

--- Challenges/MostCommonWord/MostCommonWord/Program.cs
+++ Challenges/MostCommonWord/MostCommonWord/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Hashtable.Classes;
 
 namespace MostCommonWord
 {
@@ -6,12 +7,69 @@ namespace MostCommonWord
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("In a galaxy far far away");
+            Console.WriteLine(MostCommonWord("In a galaxy far far away"));
+
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------------------");
+
+            Console.WriteLine("Taco cat ate a taco");
+            Console.WriteLine(MostCommonWord("Taco cat ate a taco"));
+
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------------------");
+
+            Console.WriteLine("No. Try not. Do or do not. There is no try.");
+            Console.WriteLine(MostCommonWord("No. Try not. Do or do not. There is no try."));
+
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------------------");
+
+            Console.ReadLine();
+
         }
 
+        /// <summary>
+        /// Takes in a string book and returns the most commonly used word in the book. A word is a substring with the book seperated by spaces.
+        /// </summary>
+        /// <param name="book"></param>
+        /// <returns>string most commonly used word in book</returns>
         public static string MostCommonWord(string book)
         {
+            //Container that will hold most common word.
             string mostCommonWord = "No Words in Book";
+            //Counter that will keep track of highest counter returned
+            int highestCounter = 0;
+            //Splits book into an array of words.
+            string[] words = book.Split(" ");
+            // Will keep track of words found in book. 
+            HashTable wordTracker = new HashTable(1024);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                // If wordtracker contains the word then it will be pulled out of storage and its counter will be increased by one then the counter will be compared to the highestCounter. 
+                if (wordTracker.Contains(words[i]))
+                {
+                    int counter = int.Parse(wordTracker.Storage[wordTracker.Hasher(words[i])].Value.ToString());
+                    counter = counter + 1;
+
+                    //If the counter is higher then the mostCommonWord will become that word and the highest counter will become the counter.
+                    if (counter > highestCounter)
+                    {
+                        mostCommonWord = wordTracker.Storage[wordTracker.Hasher(words[i])].Key;
+                        highestCounter = counter;
+                    }
+                    // Updates counter in storage
+                    wordTracker.Storage[wordTracker.Hasher(words[i])].Value = counter;
+                }
+                
+                //Adds new word to wordtracker and sets its counter to 1
+                wordTracker.Add(words[i].ToLower(), 1);
+                
+                // Handles only unique words being input in the book.
+                if (highestCounter == 0)
+                {
+                    mostCommonWord = words[i];
+                    highestCounter = 1;
+                }
+            }
 
             return mostCommonWord;
         }
--- Challenges/MostCommonWord/XUnitTestProject1/MostCommonWordTestSuite.cs
+++ Challenges/MostCommonWord/XUnitTestProject1/MostCommonWordTestSuite.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using MostCommonWord;
 
 namespace XUnitTestProject1
 {
@@ -8,33 +9,21 @@ namespace XUnitTestProject1
         [Fact]
         public void TestingOneDoubleRepeatingWord()
         {
-
-
-
+            Assert.Equal("far", Program.MostCommonWord("In a galaxy far far away"));
         }
 
         [Fact]
-        public void TestingTwoeDoubleRepeatingWords()
+        public void TestingThreeDoubleRepeatingWords()
         {
-
-
-
+            Assert.Equal("do", Program.MostCommonWord("No. Try not. Do or do not. There is no try."));
         }
 
         [Fact]
         public void TestingAllUniqueWords()
         {
-
-
-
+            Assert.Equal("I", Program.MostCommonWord("I am unique"));
         }
 
-        [Fact]
-        public void TestingNoWords()
-        {
-
-
-
-        }
+       
     }
 }
