cbc6d555ee32a94568b5db1067dc60a15f4911af
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-22 19:03:21 -0800

    Added properties and constructor

--- DataStructures/Graph/Graph/Graph.cs
+++ DataStructures/Graph/Graph/Graph.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Graph
 {
@@ -6,10 +7,18 @@ namespace Graph
     {
 
         //Size
+        public int Size { get; set; }
 
         //AdjacencyList
+        public Dictionary<Vertex, LinkedList<Edge>> _AdjacencyList { get; set; }
 
         //Constructor
+        public Graph()
+        {
+            Dictionary<Vertex, LinkedList<Edge>> adjacencyList = new Dictionary<Vertex, LinkedList<Edge>>();
+            _AdjacencyList = adjacencyList;
+            Size = 0;
+        }
 
         //Add Vertex
 
