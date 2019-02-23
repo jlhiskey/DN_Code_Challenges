c0ef66c3c8fb259b6ffe25151c00118ad29cc6a9
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-22 18:45:13 -0800

    Completed Edge Class

--- DataStructures/Graph/Graph/Edge.cs
+++ DataStructures/Graph/Graph/Edge.cs
@@ -4,12 +4,21 @@ using System.Text;
 
 namespace Graph
 {
-    class Edge
+    public class Edge
     {
         //Vertex
+        public Vertex Vertex { get; set; }
         
         //Weight
+        public int Weight { get; set; }
 
         //Constructor
+        public Edge(Vertex vertex, int weight)
+        {
+            Vertex = vertex;
+
+            Weight = weight;
+
+        }
     }
 }
