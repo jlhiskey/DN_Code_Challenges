e78b94a7650afcb1c48d0b204da65abea605e817
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-27 06:30:37 -0800

    Refactored and commented

--- Challenges/GetEdge/GetEdge/Program.cs
+++ Challenges/GetEdge/GetEdge/Program.cs
@@ -34,54 +34,63 @@ namespace GetEdge
             Console.ReadLine();
         }
 
+        /// <summary>
+        /// Accepts a graph and two string inputs of startCity and endCity. Checks if the cities exist and returns a string indicating if a direct flight can be found between the two cities.
+        /// </summary>
+        /// <param name="cities"></param>
+        /// <param name="startingCity"></param>
+        /// <param name="endingCity"></param>
+        /// <returns>String indicating is a direct flight can be found.</returns>
         public static string GetEdge(Graph.Graph cities, string startingCity, string endingCity)
         {
+            //Grab all existing cities
             List<Vertex> allCities = cities.GetVerticies();
-            bool startExists = false;
+
+            //Containers to hold cities once they are found.
             Vertex foundStart = null;
-            bool endExists = false;
-            Vertex foundFinish = null;
+            Vertex foundEnd = null;
+
+            //Searching cities for start and end cities and populates foundStart and foundEnd if found.
             foreach (Vertex city in allCities)
             {
                 if (city.Value == startingCity)
                 {
-                    startExists = true;
                     foundStart = city;
                 }
 
                 if (city.Value == endingCity)
                 {
-                    endExists = true;
-                    foundFinish = city;
+                    foundEnd = city;
                 }
             }
 
-            if(!startExists && !endExists)
+            //Handles missing cities
+            if(foundStart == null && foundEnd == null)
             {
                 return "Starting and Ending Cities Do Not Exist";
             }
-            if (!startExists)
+            if (foundStart == null)
             {
                 return "Starting City Does Not Exist";
             }
-            if (!endExists)
+            if (foundEnd == null)
             {
                 return "Ending City Does Not Exist";
             }
-            
-            if(foundStart != null)
-            {
-                List<Edge> neighbors = cities.GetNeighbors(foundStart);
 
-                foreach (Edge neighbor in neighbors)
+            //Grabs neighbors of start city
+            List<Edge> neighbors = cities.GetNeighbors(foundStart);
+
+            //Searches neighbors for end city and returns success if found.
+            foreach (Edge neighbor in neighbors)
+            {
+                if (neighbor.Vertex.Value == endingCity)
                 {
-                    if (neighbor.Vertex.Value == endingCity)
-                    {
-                        return $"Direct Route Found, Cost = ${neighbor.Weight}";
-                    }
+                    return $"Direct Route Found, Cost = ${neighbor.Weight}";
                 }
             }
-            
+           
+            //If direct route is not found returns failure.
             return "No Direct Route Found";
         }
     }
