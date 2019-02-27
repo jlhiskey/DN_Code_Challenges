b0ee22976987257a06158c7100824ea4a8e3818d
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-26 16:21:53 -0800

    Added framed out app and test app.

---
+++ Challenges/GetEdge/GetEdge/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Graph;
+
+namespace GetEdge
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public string GetEdge(Graph.Graph cities, string startingCity, string endingCity)
+        {
+
+            return "No Direct Route Found";
+        }
+    }
+}
+;
