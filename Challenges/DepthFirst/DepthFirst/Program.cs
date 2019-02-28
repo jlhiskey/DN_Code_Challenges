e49454fea608a871531037b6bb30b351326591b3
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-28 08:18:51 -0800

    Completed Challenge

--- Challenges/DepthFirst/DepthFirst/Program.cs
+++ Challenges/DepthFirst/DepthFirst/Program.cs
@@ -1,14 +1,80 @@
-﻿using System;
+﻿using Graph;
+using System;
+using System.Collections.Generic;
 
 namespace DepthFirst
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
 
+        public static List<Vertex> DepthFirst(Graph.Graph graph)
+        {
+            List<Vertex> preOrder = new List<Vertex>();
+            List<Vertex> allVerticies = graph.GetVerticies();
+            HashSet<Vertex> visitedVerticies = new HashSet<Vertex>();
+            Stack<Vertex> stack = new Stack<Vertex>();
+            Vertex start = null;
 
+            Vertex FindLowestVertex(List<Vertex> listOfVerticies)
+            {
+                Vertex lowestVertex = null;
+                int lowestValue = 0;
+
+                foreach(Vertex vertex in listOfVerticies)
+                {
+                    if(!visitedVerticies.Contains(vertex))
+                    {
+                        if (lowestVertex == null)
+                        {
+                            lowestVertex = vertex;
+                            lowestValue = int.Parse(vertex.Value.ToString());
+                        }
+                        if(int.Parse(vertex.Value.ToString()) < lowestValue)
+                        {
+                            lowestVertex = vertex;
+                            lowestValue = int.Parse(vertex.Value.ToString());
+                        }
+                    }
+                    return null;
+                    
+                }
+                return lowestVertex;
+            }
+
+            start = FindLowestVertex(allVerticies);
+
+            stack.Push(start);
+
+            visitedVerticies.Add(start);
+
+            while(stack.Count > 0)
+            {
+                Vertex target = stack.Pop();
+
+                preOrder.Add(target);
+
+                List<Edge> edgeNeighbors = graph.GetNeighbors(target);
+
+                List<Vertex> vertexNeighbors = new List<Vertex>();
+
+                foreach(Edge edge in edgeNeighbors)
+                {
+                    vertexNeighbors.Add(edge.Vertex);
+                }
+
+                while(FindLowestVertex(vertexNeighbors) != null)
+                {
+                    Vertex addingToStack = FindLowestVertex(vertexNeighbors);
+                    stack.Push(addingToStack);
+                    visitedVerticies.Add(addingToStack);
+                    vertexNeighbors.Remove(addingToStack);
+                }
+            }
+            return preOrder;
+        }
     }
 }
--- Challenges/DepthFirst/DepthFirstTestSuite/UnitTest1.cs
+++ Challenges/DepthFirst/DepthFirstTestSuite/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Graph;
 using System;
 using Xunit;
 
@@ -8,6 +9,25 @@ namespace DepthFirstTestSuite
         [Fact]
         public void Test1()
         {
+            Graph.Graph graph = new Graph.Graph();
+            Vertex one = new Vertex(1);
+            Vertex two = new Vertex(2);
+            Vertex three = new Vertex(3);
+            Vertex four = new Vertex(4);
+            Vertex five = new Vertex(5);
+
+            graph.AddVertex(one);
+            graph.AddVertex(two);
+            graph.AddVertex(three);
+            graph.AddVertex(four);
+            graph.AddVertex(five);
+
+            graph.AddEdge(one, two);
+            graph.AddEdge(two, five);
+            graph.AddEdge(two, three);
+            graph.AddEdge(three, four);
+
+            List<Vertex> 
 
         }
     }
