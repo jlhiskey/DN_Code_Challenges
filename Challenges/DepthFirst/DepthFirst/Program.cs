44dc5bff0ea755559584b21733019f264cbb6449
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-28 07:50:44 -0800

    Framed out project files

---
+++ Challenges/DepthFirst/DepthFirst/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DepthFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+}
